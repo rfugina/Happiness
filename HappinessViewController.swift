7e40db4d70198786c9eaf4a718fd399f7de8f3d5
Rob Fugina <robf@fugina.com>
2015-03-08 23:47:17 -0500

    Happiness app from Lecture 5

---
+++ HappinessViewController.swift
@@ -0,0 +1,15 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by Rob Fugina on 3/8/15.
+//  Copyright (c) 2015 Rob Fugina. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController {
+
+
+
+}
